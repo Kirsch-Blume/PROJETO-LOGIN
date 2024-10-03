f2c64cba02a6b8a15c3a928c1d22de71de5bc9e8
Miguel <apolianamaraiza@gmail.com>
2024-10-03 17:12:11 -0300

    Modo escuro implementado, checkbox de mostrar senha implementada, alertas de sucesso e erro no login implementado e alterações no CSS.

--- main.js
+++ main.js
@@ -1,20 +1,49 @@
-let usuario = document.querySelector('#usuario')
-let senha = document.querySelector('#senha')
+let body = document.querySelector("body");
+let usuario = document.querySelector("#usuario");
+let senha = document.querySelector("#senha");
 let usuarioCerto = "admin";
-let senhaCerta ="123";
-let form = document.querySelector("#login-form")
+let senhaCerta = "123";
+let form = document.querySelector("#login-form");
+let msg = document.querySelector("#mensagem");
+let checkbox = document.querySelector("#mostrar-senha");
+let darkMode = document.querySelector("#dark-mode");
 
-function login(event){
-    event.preventDefault();
-   
+function login(event) {
+  event.preventDefault();
 
-    if(usuario.value == usuarioCerto && senha.value == senhaCerta){
-        alert('sucesso');
-        form.reset();
-    }else{
-        alert('Usuário ou senha incorreto(a)');
-    }
-    
+  msg.className = "";
+  if (usuario.value == usuarioCerto && senha.value == senhaCerta) {
+    msg.textContent = "Login realizado com sucesso";
+    msg.classList.toggle("sucesso");
+    form.reset();
+  } else {
+    msg.textContent = "Senha/Usuário incorreto(a)";
+    msg.classList.toggle("erro");
+  }
+  //removendo msg apos x segundos
+
+  setTimeout(() => {
+    msg.className = "";
+  }, 2000);
 }
+//Mostrando ou ocupando a senha do usuário
+checkbox.addEventListener("click", () => {
+  if (senha.type == "password") {
+    senha.type = "text";
+  } else {
+    senha.type = "password";
+  }
+});
 
-form.addEventListener("submit", login)
+darkMode.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  form.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    darkMode.textContent = "🌞";
+  } else {
+    darkMode.textContent = "🌙";
+  }
+});
+
+form.addEventListener("submit", login);
